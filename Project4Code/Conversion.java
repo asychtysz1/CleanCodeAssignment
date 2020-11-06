5eaf9f59bbc0040efc4b83e3fad10cc7c0d7e25c
asychtysz1 <asychtysz1@worcester.edu>
2020-11-06 18:03:48 -0500

    Fix #3, Changed return of Conversion method

--- Project4Code/Conversion.java
+++ Project4Code/Conversion.java
@@ -12,111 +12,111 @@ public class Conversion   {
      * @return String variable "result" here the converted version of roman is stored
      */
     public static String Convert(int userInput){
-        String result = "";
-        int currentNum = userInput;  
+        String convertedResult = "";
+        int originalUserInput = userInput;  
         do {
            if (userInput >= 1000)    {
-               result += "M";
+        	   convertedResult += "M";
                userInput -= 1000;
            }
            else if(userInput >= 900) {
-               result += "CM";
+        	   convertedResult += "CM";
                userInput -= 900;
            }
            else if(userInput >= 800) {
-               result += "DCCC";
+        	   convertedResult += "DCCC";
                userInput -= 800;
            }
            else if(userInput >= 700) {
-               result += "DCC";
+        	   convertedResult += "DCC";
                userInput -= 700;
            }
            else if(userInput >= 600) {
-               result += "DC";
+        	   convertedResult += "DC";
                userInput -= 600;
            }
            else if(userInput >= 500) {    
-               result += "D";
+        	   convertedResult += "D";
                userInput -= 500;
            }
            else if(userInput >= 400) {
-               result += "CD";
+        	   convertedResult += "CD";
                userInput -= 400;
            }
            else if(userInput >= 300) {
-               result += "CCC";
+        	   convertedResult += "CCC";
                userInput -= 300;
            }
            else if(userInput >= 200) {
-               result += "CC";
+        	   convertedResult += "CC";
                userInput -= 200;
            }
            else if(userInput >= 100) {
-               result += "C";
+        	   convertedResult += "C";
                userInput -= 100;
            }
            else if(userInput >= 90) {
-               result += "XC";
+        	   convertedResult += "XC";
                userInput -= 90;
            }
            else if(userInput >= 80) {
-               result += "LXXX";
+        	   convertedResult += "LXXX";
                userInput -= 80;
            }
            else if(userInput >= 70) {
-               result += "LXX";
+        	   convertedResult += "LXX";
                userInput -= 70;
            }
            else if(userInput >= 60) {
-               result += "LX";
+        	   convertedResult += "LX";
                userInput -= 60;
            }
            else if(userInput >= 50) {
-               result += "L";
+        	   convertedResult += "L";
                userInput -= 50;
            }else if(userInput >= 40) {
-               result += "XL";
+        	   convertedResult += "XL";
                userInput -= 40;
            }
            else if(userInput >= 10) {
-               result += "X";
+        	   convertedResult += "X";
                userInput -= 10;
            }
            else if(userInput == 9) {
-               result += "IX";
+        	   convertedResult += "IX";
                userInput -= 9;
            }else if(userInput == 8) {
-               result += "VIII";
+        	   convertedResult += "VIII";
                userInput -= 8;
            }
            else if(userInput == 7) {
-               result += "VII";
+        	   convertedResult += "VII";
                userInput -= 7;
            }
            else if(userInput == 6) {
-               result += "VI";
+        	   convertedResult += "VI";
                userInput -= 6;
            }
            else if(userInput == 5) {
-               result += "V";
+        	   convertedResult += "V";
                userInput -= 5;
            }
            else if(userInput == 4) {
-               result += "IV";
+        	   convertedResult += "IV";
                userInput -= 4;
            }else if(userInput == 3) {
-               result += "III";
+        	   convertedResult += "III";
                userInput -= 3;
            } 
            else if(userInput == 2) {
-               result += "II";
+        	   convertedResult += "II";
                userInput -= 2;
            }
            else if(userInput == 1) {
-               result += "I";
+        	   convertedResult += "I";
                userInput -= 1;
            }
         } while (userInput > 0);
-        return (currentNum + " in Roman numerals is " + result);  
+        return (originalUserInput + " in Roman numerals is " + convertedResult);  
     }
 }    
